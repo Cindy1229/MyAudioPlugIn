c1bf46c902abdf49fb59a58ecef07044105d7e8f
Cindy1229 <leticiashi3@gmail.com>
2021-08-17 13:03:39 -0400

    refactored peakfilter

--- SimpleEQ/Source/PluginProcessor.cpp
+++ SimpleEQ/Source/PluginProcessor.cpp
@@ -228,10 +228,16 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts) {
     return settings;
 }
 
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate)
+{
+    return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate, chainSettings.peakFreq, chainSettings.peakQuality, juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
 void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings)
 {
     
-    auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(), chainSettings.peakFreq, chainSettings.peakQuality, juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+    
+    auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
     
     
     updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
@@ -239,7 +245,7 @@ void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings
     
 }
 
-void SimpleEQAudioProcessor::updateCoefficients(Coefficients &old, const Coefficients &replacements)
+void updateCoefficients(Coefficients &old, const Coefficients &replacements)
 {
     *old = *replacements;
     
